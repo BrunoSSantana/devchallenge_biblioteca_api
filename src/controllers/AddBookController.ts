bb425b4cb0dfdac4b4683bb6773e4763528b0f07
BrunoSSantana <brunoosouza15@gmail.com>
2021-11-16 15:43:46 -0300

    refactor addBook useCase

--- src/controllers/AddBookController.ts
+++ src/controllers/AddBookController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
 import { BooksRepository } from "../repositories/BooksRepository";
 import { AddBookService } from "../services/AddBookService";
 
 class AddBookController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const addBookService = new AddBookService()
+    const booksRepository = getCustomRepository(BooksRepository)
+    const addBookService = new AddBookService(booksRepository)
     
     const { autores, editora, foto, titulo } = request.body
 
--- src/services/AddBookService.ts
+++ src/services/AddBookService.ts
@@ -1,12 +1,12 @@
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Repository } from "typeorm";
+import { Book } from "../entities/Book";
 import { BooksRepository } from "../repositories/BooksRepository";
 
 class AddBookService {
-
+  constructor(private repository: Repository<Book>) { }
   async execute(autores: string[], editora: string, foto: string, titulo: string) {
-    const booksRepository = getCustomRepository(BooksRepository)
-    const book = booksRepository.create({ autores, editora, foto, titulo })
-    await booksRepository.save(book)
+    const book = this.repository.create({ autores, editora, foto, titulo })
+    await this.repository.save(book)
 
     return book
   }
